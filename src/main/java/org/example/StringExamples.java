19a57fe7def93a96d255ee41f0054a42aeaa4c08
bhavin-perennial <bhavin.majithia@perennialsys.com>
2022-07-12 11:29:45 +0530

    add String immutability example

--- src/main/java/org/example/StringExamples.java
+++ src/main/java/org/example/StringExamples.java
@@ -12,5 +12,11 @@ public class StringExamples {
         String s3 = s2.intern();
         System.out.println(s1==s3);//true
         System.out.println(s2==s3);//false
+
+        //DEMOSTRATE STRING IMMUTABILITY
+        String str1 ="Hello";
+        System.out.println(str1.hashCode());
+        str1+="World";
+        System.out.println(str1.hashCode());
     }
 }
